5c8f32c99c55aded8d96ce1fb65b3ebbe5679dcc
Wesley Brito <wesley.brito@aisdigital.com.br>
2020-11-01 15:02:58 -0300

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Wesley Brito on 01/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
